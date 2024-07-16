8b82bad09eafa5c0b62a24e57525f80ed6b337ea
StanciuIulian <dinno_1981@yahoo.com>
2024-07-16 12:26:00 +0300

    run code formatter

--- trips_ex.py
+++ trips_ex.py
@@ -6,11 +6,11 @@ from datetime import datetime
 
 
 @click.command()
-@click.argument('csv_file', type=click.Path(exists=True))
+@click.argument("csv_file", type=click.Path(exists=True))
 def csv_to_dict_list(csv_file):
     elemente = []
     try:
-        with open(csv_file, mode='r', newline='', encoding='utf-8') as file:
+        with open(csv_file, mode="r", newline="", encoding="utf-8") as file:
             reader = csv.DictReader(file)
             for row in reader:
                 elemente.append(dict(row))
@@ -21,10 +21,27 @@ def csv_to_dict_list(csv_file):
 
 class Calatorie:
 
-    def __init__(self, cost_total, curse_anulate, curse_complete, curse_totale, curse_orase, distanta_totala,
-                 curse_produs, extragere_date, timp_inceput_cursa, timp_sfarsit_cursa, total_timp_petrecut,
-                 total_timp_secunde, timp_petrecut_in_minute, timp_petrecut_in_ore, timp_petrecut_in_zile,
-                 cea_mai_scurta_cursa, cea_mai_lunga_cursa, curse_an ):
+    def __init__(
+        self,
+        cost_total,
+        curse_anulate,
+        curse_complete,
+        curse_totale,
+        curse_orase,
+        distanta_totala,
+        curse_produs,
+        extragere_date,
+        timp_inceput_cursa,
+        timp_sfarsit_cursa,
+        total_timp_petrecut,
+        total_timp_secunde,
+        timp_petrecut_in_minute,
+        timp_petrecut_in_ore,
+        timp_petrecut_in_zile,
+        cea_mai_scurta_cursa,
+        cea_mai_lunga_cursa,
+        curse_an,
+    ):
         self.cost_total = cost_total
         self.curse_anulate = curse_anulate
         self.curse_complete = curse_complete
@@ -44,25 +61,34 @@ class Calatorie:
         self.cea_mai_lunga_cursa = cea_mai_lunga_cursa
         self.curse_an = curse_an
 
-
     def trip_cost(self):
-        cost_ron = list(cost['Fare Amount'] for cost in elemente if cost['Fare Currency'] == 'RON')
-        cost_euro = list(cost['Fare Amount'] for cost in elemente if cost['Fare Currency'] == 'EUR')
+        cost_ron = list(
+            cost["Fare Amount"] for cost in elemente if cost["Fare Currency"] == "RON"
+        )
+        cost_euro = list(
+            cost["Fare Amount"] for cost in elemente if cost["Fare Currency"] == "EUR"
+        )
         cost_total_ron = sum(cost_ron)
         cost_total_euro = sum(cost_euro) * 5
         cost_total = cost_total_ron + cost_total_euro
         return cost_total
 
     def trip_canceled(self):
-        curse_anulate = Counter(curse['Trip or Order Status'] for curse in elemente if
-                                curse['Trip or Order Status'] == 'CANCELED')
-        curse_anulate = curse_anulate['CANCELED']
+        curse_anulate = Counter(
+            curse["Trip or Order Status"]
+            for curse in elemente
+            if curse["Trip or Order Status"] == "CANCELED"
+        )
+        curse_anulate = curse_anulate["CANCELED"]
         return curse_anulate
 
     def trip_completed(self):
-        curse_complete = Counter(curse['Trip or Order Status'] for curse in elemente if
-            curse['Trip or Order Status'] == 'COMPLETED')
-        curse_complete = curse_complete['COMPLETED']
+        curse_complete = Counter(
+            curse["Trip or Order Status"]
+            for curse in elemente
+            if curse["Trip or Order Status"] == "COMPLETED"
+        )
+        curse_complete = curse_complete["COMPLETED"]
         return curse_complete
 
     def total_trips(self):
@@ -72,53 +98,71 @@ class Calatorie:
         return curse_totale
 
     def city_trips(self):
-        curse_orase = list(Counter(curse['City'] for curse in elemente if curse['City'] == str(curse['City'])).items())
+        curse_orase = list(
+            Counter(
+                curse["City"]
+                for curse in elemente
+                if curse["City"] == str(curse["City"])
+            ).items()
+        )
         return curse_orase
 
     def total_diatance(self):
-        distanta_mile = list(curse['Distance (miles)'] for curse in elemente if
-                             curse['Trip or Order Status'] == 'COMPLETED' for curse in elemente)
-        distanta_mile = [elem for elem in distanta_mile if elem != '']
+        distanta_mile = list(
+            curse["Distance (miles)"]
+            for curse in elemente
+            if curse["Trip or Order Status"] == "COMPLETED"
+            for curse in elemente
+        )
+        distanta_mile = [elem for elem in distanta_mile if elem != ""]
         distanta_totala = sum(distanta_mile) * 1.6
         return distanta_totala
 
     def product_type(self):
-        curse_produs = list(Counter(curse['Product Type'] for curse in elemente).items())
+        curse_produs = list(
+            Counter(curse["Product Type"] for curse in elemente).items()
+        )
         return curse_produs
 
     def begin_trip_time(self):
-        inceput_cursa = list(curse['Begin Trip Time'] for curse in elemente)
+        inceput_cursa = list(curse["Begin Trip Time"] for curse in elemente)
         timp_inceput_cursa = []
         for timp in inceput_cursa:
             # aici am avut o cursa care incepea in data de 2023-04-10 si se termina in 1970-01-01
-            if timp == '2023-04-10 10:11:31 +0000 UTC':
+            if timp == "2023-04-10 10:11:31 +0000 UTC":
                 continue
-            dt = datetime.strptime(timp, '%Y-%m-%d %H:%M:%S %z %Z')
+            dt = datetime.strptime(timp, "%Y-%m-%d %H:%M:%S %z %Z")
             if dt.year == 1970:
                 continue
-            timestamp = dt.strftime('%Y-%m-%d %H:%M:%S')
+            timestamp = dt.strftime("%Y-%m-%d %H:%M:%S")
             timestamp_in_seconds = int(dt.timestamp())
-            timp_inceput_cursa.append({dt.year: timestamp, 'seconds': timestamp_in_seconds})
+            timp_inceput_cursa.append(
+                {dt.year: timestamp, "seconds": timestamp_in_seconds}
+            )
         return timp_inceput_cursa
 
     def dropoff_trip_time(self):
-        sfarsit_cursa = list(curse['Dropoff Time'] for curse in elemente)
+        sfarsit_cursa = list(curse["Dropoff Time"] for curse in elemente)
         timp_sfarsit_cursa = []
         for timp in sfarsit_cursa:
-            dt = datetime.strptime(timp, '%Y-%m-%d %H:%M:%S %z %Z')
+            dt = datetime.strptime(timp, "%Y-%m-%d %H:%M:%S %z %Z")
             if dt.year == 1970:
                 continue
-            timestamp = dt.strftime('%Y-%m-%d %H:%M:%S')
+            timestamp = dt.strftime("%Y-%m-%d %H:%M:%S")
             timestamp_in_seconds = int(dt.timestamp())
-            timp_sfarsit_cursa.append({dt.year: timestamp, 'seconds': timestamp_in_seconds})
+            timp_sfarsit_cursa.append(
+                {dt.year: timestamp, "seconds": timestamp_in_seconds}
+            )
         return timp_sfarsit_cursa
 
     def time_spent_trips(self):
         timp_inceput_cursa = calatorie.begin_trip_time()
         timp_sfarsit_cursa = calatorie.dropoff_trip_time()
-        timp_inceput = [cursa['seconds'] for cursa in timp_inceput_cursa]
-        timp_sfarsit = [cursa['seconds'] for cursa in timp_sfarsit_cursa]
-        timp_petrecut_calatorii = [val1 - val2 for val1, val2 in zip(timp_sfarsit, timp_inceput)]
+        timp_inceput = [cursa["seconds"] for cursa in timp_inceput_cursa]
+        timp_sfarsit = [cursa["seconds"] for cursa in timp_sfarsit_cursa]
+        timp_petrecut_calatorii = [
+            val1 - val2 for val1, val2 in zip(timp_sfarsit, timp_inceput)
+        ]
         return timp_petrecut_calatorii
 
     def time_spent_seconds(self):
@@ -165,14 +209,26 @@ class Calatorie:
 
 
 elemente = csv_to_dict_list()
-calatorie = Calatorie('cost_total', 'curse_anulate', 'curse_complete',
-                      'curse_totale', 'curse_orase', 'distanta_totala',
-                      'curse_produs','extragere_date', 'timp_inceput_cursa',
-                      'timp_sfarsit_cursa','total_timp_petrecut',
-                      'total_timp_secunde', 'timp_petrecut_in_minute',
-                      'timp_petrecut_in_ore', 'timp_petrecut_in_zile',
-                      'cea_mai_scurta_cursa', 'cea_mai_lunga_cursa',
-                      'curse_an' )
+calatorie = Calatorie(
+    "cost_total",
+    "curse_anulate",
+    "curse_complete",
+    "curse_totale",
+    "curse_orase",
+    "distanta_totala",
+    "curse_produs",
+    "extragere_date",
+    "timp_inceput_cursa",
+    "timp_sfarsit_cursa",
+    "total_timp_petrecut",
+    "total_timp_secunde",
+    "timp_petrecut_in_minute",
+    "timp_petrecut_in_ore",
+    "timp_petrecut_in_zile",
+    "cea_mai_scurta_cursa",
+    "cea_mai_lunga_cursa",
+    "curse_an",
+)
 cost_total = calatorie.trip_cost()
 curse_anulate = calatorie.trip_canceled()
 curse_complete = calatorie.trip_completed()
@@ -209,6 +265,5 @@ print(f"Cea mai scurta cursa {cea_mai_scurta_cursa} minute")
 print(f"Cea mai lunga cursa {cea_mai_lunga_cursa} minute")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     csv_to_dict_list()
-
